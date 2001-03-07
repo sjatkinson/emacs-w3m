c6c4877e48217396139bdba772ad43000fe65f88
anonymous <anonymous>
2001-03-07 22:45:54 +0000

    * w3m.el (w3m-search-engine-alist): Added "rpmfind".

--- w3m.el
+++ w3m.el
@@ -72,7 +72,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.76 $"))
+    (let ((rev "$Revision: 1.77 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "0.2.%d"
 		   (- (string-to-number (match-string 1 rev)) 28)))))
@@ -359,7 +359,8 @@ MIME CHARSET and CODING-SYSTEM must be symbol."
     ("yahoo-ja" "http://search.yahoo.co.jp/bin/search?p=%s" euc-japan)
     ("google" "http://www.google.com/search?q=%s" nil)
     ("google-ja" "http://www.google.com/search?q=%s&hl=ja&lr=" shift_jis)
-    ("goo-ja" "http://www.goo.ne.jp/default.asp?MT=%s" euc-japan))
+    ("goo-ja" "http://www.goo.ne.jp/default.asp?MT=%s" euc-japan)
+    ("rpmfind" "http://rpmfind.net/linux/rpm2html/search.php?query=%s"))
   "*An alist of search engines.
 Each elemnt looks like (ENGINE ACTION CODING)
 ENGINE is a string, the name of the search engine.
