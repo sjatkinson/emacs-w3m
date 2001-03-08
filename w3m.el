dc43126f6f8c06459c623e6fdebb9173a1d175d0
teranisi <teranisi>
2001-03-08 22:40:19 +0000

    * w3m.el (w3m-toggle-inline-images): Eliminated needless local variable.  (toplevel): Encloses old emacs judgement with eval-and-compile;  Define `find-coding-system' using `w3m-static-if'.

--- w3m.el
+++ w3m.el
@@ -44,24 +44,17 @@
 
 ;;; Code:
 
-(or (and (boundp 'emacs-major-version)
-	 (>= emacs-major-version 20))
-    (progn
-      (require 'poe)
-      (require 'pcustom)))
+(eval-and-compile
+  (or (and (boundp 'emacs-major-version)
+	   (>= emacs-major-version 20))
+      (progn
+	(require 'poe)
+	(require 'pcustom))))
 
 (if (featurep 'xemacs)
     (require 'poem))
 
 (require 'thingatpt)
-
-(eval-and-compile
-  (unless (fboundp 'find-coding-system)
-    (if (fboundp 'coding-system-p)
-	(defsubst find-coding-system (obj)
-	  "Return OBJ if it is a coding-system."
-	  (if (coding-system-p obj) obj))
-      (require 'pces))))
 
 ;; this package using a few CL macros
 (eval-when-compile (require 'cl))
@@ -70,9 +63,16 @@
 (defmacro w3m-static-if (cond then &rest else)
   (if (eval cond) then (` (progn  (,@ else)))))
 
+(w3m-static-if (not (fboundp 'find-coding-system))
+    (w3m-static-if (fboundp 'coding-system-p)
+	(defsubst find-coding-system (obj)
+	  "Return OBJ if it is a coding-system."
+	  (if (coding-system-p obj) obj))
+      (require 'pces)))
+
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.84 $"))
+    (let ((rev "$Revision: 1.85 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "0.2.%d"
 		   (- (string-to-number (match-string 1 rev)) 28)))))
@@ -1213,7 +1213,7 @@ If second optional argument NO-CACHE is non-nil, cache is not used."
   (unless (and force (eq w3m-display-inline-image-status 'on))
     (let ((cur-point (point)) 
 	  (buffer-read-only)
-	  point end url image delim)
+	  point end url image)
       (if (or force (eq w3m-display-inline-image-status 'off))
 	  (save-excursion
 	    (goto-char (point-min))
