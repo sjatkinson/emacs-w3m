dd37657456cedaa064d82882a5647237d1915564
ari <ari>
2001-03-15 15:30:00 +0000

    * w3m.el (w3m-view-source): Render as html if already display source.

--- w3m.el
+++ w3m.el
@@ -97,7 +97,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.119 $"))
+    (let ((rev "$Revision: 1.120 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "0.2.%d"
 		   (- (string-to-number (match-string 1 rev)) 28)))))
@@ -2194,7 +2194,10 @@ ex.) c:/dir/file => //c/dir/file"
 (defun w3m-view-source ()
   "*Display source of this current buffer."
   (interactive)
-  (w3m-goto-url (concat "about://source/" w3m-current-url)))
+  (w3m-goto-url
+   (if (string-match "^about://source/" w3m-current-url)
+       (substring w3m-current-url (match-end 0))
+     (concat "about://source/" w3m-current-url))))
 
 (defun w3m-about-header (url &optional no-decode accept-type-regexp no-cache)
   (when (string-match "^about://header/" url)
