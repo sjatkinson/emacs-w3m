1b4f0b193bbe602e8b1fd5313e0716ac8fda69ab
shirai <shirai>
2001-05-31 01:42:24 +0000

    * w3m-dtree.el (w3m-dtree-create): Insert 'Meta tag' to the binding of charset.
    * w3m.el (w3m-local-dirlist-cgi): Fix charset handling.

--- w3m.el
+++ w3m.el
@@ -107,7 +107,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.292 $"))
+    (let ((rev "$Revision: 1.293 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "0.2.%d"
 		   (- (string-to-number (match-string 1 rev)) 28)))))
@@ -482,7 +482,7 @@ It will be used for the w3m system internal for Emacs 21.")
       (or (w3m-find-coding-system (car (car rest)))
 	  (setq dest (cons (car rest) dest)))
       (setq rest (cdr rest)))
-    (nreverse dest))
+    dest)
   "Alist MIME CHARSET vs CODING-SYSTEM.
 MIME CHARSET and CODING-SYSTEM must be symbol."
   :group 'w3m
@@ -1923,7 +1923,7 @@ to nil."
   ;; bind charset to w3m-file-name-coding-system
   (let ((charset (or (car (rassq w3m-file-name-coding-system
 				 w3m-charset-coding-system-alist))
-		     'unknown)))
+		     w3m-file-name-coding-system)))
     (goto-char (point-min))
     (when (re-search-forward "<head>" nil t)
       (insert "\n<meta http-equiv=\"CONTENT-TYPE\" "
