af9aeb82fadffe8841e0b505c4ae05d6432a1e21
yamaoka <yamaoka>
2001-10-14 23:46:35 +0000

    * w3m.el (w3m-entity-regexp): Unified to the use of `regexp-opt';  autoload regexp-opt for old Emacsen.

--- w3m.el
+++ w3m.el
@@ -127,7 +127,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.430 $"))
+    (let ((rev "$Revision: 1.431 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "1.1.%d"
 		   (- (string-to-number (match-string 1 rev)) 233)))))
@@ -721,18 +721,16 @@ will disclose your private informations, for example:
 			    (w3m-static-if (boundp 'MULE) lc-ltn1 'latin-iso8859-1)
 			    (cdr entity))))))
 		 latin1-entity))))))
+
+;; Required for old Emacsen.  See the file README for details.
+(eval-and-compile
+  (autoload 'regexp-opt "regexp-opt"))
+
 (defconst w3m-entity-regexp
   (eval-when-compile
     (format "&\\(%s\\|#[0-9]+\\|#x[0-9a-f]+\\);?"
-	    (if (fboundp 'regexp-opt)
-		(let ((fn (function regexp-opt)))
-		  ;; Don't funcall directly for avoiding compile warning.
-		  (funcall fn (mapcar (function car)
-				      w3m-entity-alist)))
-	      (mapconcat (lambda (s)
-			   (regexp-quote (car s)))
-			 w3m-entity-alist
-			 "\\|")))))
+	    (regexp-opt (mapcar (function car) w3m-entity-alist)))))
+
 (defvar w3m-entity-db nil)		; nil means un-initialized
 (defconst w3m-entity-db-size 13)	; size of obarray
 
