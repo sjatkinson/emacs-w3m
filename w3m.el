21c668f719bf5e5beef345007cefce7e5b05e53c
tsuchiya <tsuchiya>
2002-01-05 06:28:59 +0000

    (w3m-url-encode-string): Add a period to the set of characters which are not encoded. From Fabrice Bauzac <noon@noon.dnsalias.net>

--- w3m.el
+++ w3m.el
@@ -124,7 +124,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.533 $"))
+    (let ((rev "$Revision: 1.534 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "1.2.%d"
 		   (- (string-to-number (match-string 1 rev)) 426)))))
@@ -1609,7 +1609,7 @@ which defaults to the value of `w3m-file-coding-system-for-read'."
 	    (cond
 	     ((eq ch ?\n)		; newline
 	      "%0D%0A")
-	     ((string-match "[-a-zA-Z0-9_:/]" (char-to-string ch)) ; xxx?
+	     ((string-match "[-a-zA-Z0-9_:/.]" (char-to-string ch)) ; xxx?
 	      (char-to-string ch))	; printable
 	     ((char-equal ch ?\x20)	; space
 	      "+")
