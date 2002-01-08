8747ff0babae43eadaf8dd03cd120fa14b7e07ec
tsuchiya <tsuchiya>
2002-01-08 10:03:08 +0000

    Small improved.

--- w3m.el
+++ w3m.el
@@ -124,7 +124,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.545 $"))
+    (let ((rev "$Revision: 1.546 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "1.2.%d"
 		   (- (string-to-number (match-string 1 rev)) 426)))))
@@ -3300,7 +3300,7 @@ also make a new frame for the copied session."
 	  (w3m-toggle-inline-image)
 	(w3m-view-image)))
      ((setq url (w3m-url-at-point))
-      (when (y-or-n-p (format "Browse <%s> ? " url))
+      (unless (eq 'quit (setq url (w3m-input-url nil url 'quit)))
 	(w3m-view-this-url-1 url arg new-session)))
      (t (message "No URL at point")))))
 
