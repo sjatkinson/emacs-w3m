225edb949820bec5906d3011beb108102627c93a
shirai <shirai>
2002-02-05 07:43:31 +0000

    (w3m-meta-charset-content-type-regexp) (w3m-meta-content-refresh-regexp): Allow broken quotation mark.

--- w3m.el
+++ w3m.el
@@ -124,7 +124,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.572 $"))
+    (let ((rev "$Revision: 1.573 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "1.2.%d"
 		   (- (string-to-number (match-string 1 rev)) 426)))))
@@ -1165,7 +1165,7 @@ for a charset indication")
 
 (defconst w3m-meta-charset-content-type-regexp
   (eval-when-compile
-    (concat "<meta[ \t]+content=\"\\([^;]+\\)"
+    (concat "<meta[ \t]+content=\"?\\([^;]+\\)"
 	    ";[ \t]*charset=\"?\\([^\"]+\\)\"?"
 	    "[ \t]+http-equiv=\"?Content-type\"?[ \t]*/?>"))
   "Regexp used in parsing `<META content=\"...;charset=...\" HTTP-EQUIV=\"Content-Type\">
@@ -1182,7 +1182,7 @@ for a refresh indication")
 
 (defconst w3m-meta-content-refresh-regexp
   (eval-when-compile
-    (concat "<meta[ \t]+content=\"\\([^;]+\\)"
+    (concat "<meta[ \t]+content=\"?\\([^;]+\\)"
 	    ";[ \t]*url=\"?\\([^\"]+\\)\"?"
 	    "[ \t]+http-equiv=\"?refresh\"?[ \t]*/?>"))
   "Regexp used in parsing `<META content=\"n;url=...\" HTTP-EQUIV=\"Refresh\">
