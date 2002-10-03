8c9e93d8d1d385821fd74bad9d9135249e9edb01
yamaoka <yamaoka>
2002-10-03 11:49:07 +0000

    * w3m-bitmap.el (w3m-create-resized-image): New function.
    * w3m-e19.el (w3m-create-resized-image): Alias to `ignore'.
    * w3m-e20.el (w3m-create-resized-image): Alias to `ignore' if the
     BITMAP-MULE is not available.
    * w3m-om.el (w3m-create-resized-image): Ditto.
    
    * w3m-image (w3m-resize-image-by-rate): Limit the minimum scale.
    
    * w3m.el (w3m-resize-inline-image-internal): Remove an existing image only
     for XEmacs.

--- w3m.el
+++ w3m.el
@@ -137,7 +137,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.728 $"))
+    (let ((rev "$Revision: 1.729 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "1.3.%d"
 		   (- (string-to-number (match-string 1 rev)) 642)))))
@@ -2470,7 +2470,8 @@ RATE is resize percentage."
 		  (if image
 		      (when (equal url w3m-current-url)
 			(let (buffer-read-only)
-			  (w3m-remove-image start end)
+			  (w3m-static-when (featurep 'xemacs)
+			    (w3m-remove-image start end))
 			  (w3m-insert-image start end image iurl))
 			;; Redisplay
 			(when w3m-force-redisplay
