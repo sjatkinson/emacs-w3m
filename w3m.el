64f55fc2340e460434af133942646125607ce368
teranisi <teranisi>
2002-10-21 15:32:14 +0000

    Fixed last change.

--- w3m.el
+++ w3m.el
@@ -138,7 +138,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.746 $"))
+    (let ((rev "$Revision: 1.747 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "1.3.%d"
 		   (- (string-to-number (match-string 1 rev)) 642)))))
@@ -5768,9 +5768,8 @@ Cannot run two w3m processes simultaneously \
     (w3m-history-store-position)
     ;; Access url group
     (if (string-match "\\`group:" url)
-	(let ((urls (split-string
-		     (w3m-url-decode-string (substring url (match-end 0)))
-		     "&")))
+	(let ((urls (mapcar 'w3m-url-decode-string 
+			    (split-string (substring url (match-end 0)) "&"))))
 	  (w3m-process-do
 	      (type (prog1
 			(w3m-goto-url (car urls))
