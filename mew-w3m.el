4ce8837176821a0397eec4ae5278392587404161
shirai <shirai>
2002-11-17 08:41:45 +0000

    * mew-w3m.el (mew-mime-text/html-w3m): Use `match-string-no-properties' if it exist.

--- mew-w3m.el
+++ mew-w3m.el
@@ -5,7 +5,7 @@
 ;; Author: Shun-ichi GOTO  <gotoh@taiyo.co.jp>,
 ;;         Hideyuki SHIRAI <shirai@meadowy.org>
 ;; Created: Wed Feb 28 03:31:00 2001
-;; Version: $Revision: 1.40 $
+;; Version: $Revision: 1.41 $
 ;; Keywords: Mew, mail, w3m, WWW, hypermedia
 
 ;; This file is a part of emacs-w3m.
@@ -153,7 +153,10 @@ This variable effected only XEmacs or Emacs 21."
 		       (goto-char (point-min))
 		       (re-search-forward "^Xref: \\(.+\\)\n" eoh t))
 	      (setq xref (match-string 1))
-	      (set-text-properties 0 (length xref) nil xref)))))
+	      (w3m-static-if (fboundp 'match-string-no-properties)
+		  (setq xref (match-string-no-properties 1))
+		(setq xref (match-string 1))
+		(set-text-properties 0 (length xref) nil xref))))))
       (mew-elet
        (cond
 	((and (null cache) (eq w3m-type 'w3m-m17n))
