767a2a82e9abe84b99221ae00de5f4ae1c965fd2
shirai <shirai>
2002-12-19 06:52:51 +0000

    (w3m-local-dirlist-cgi): Treat a local cookie.

--- w3m.el
+++ w3m.el
@@ -140,7 +140,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.772 $"))
+    (let ((rev "$Revision: 1.773 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "1.3.%d"
 		   (- (string-to-number (match-string 1 rev)) 642)))))
@@ -3194,12 +3194,17 @@ succeed."
 	  (let ((coding-system-for-read 'binary)
 		(default-process-coding-system
 		  (cons 'binary 'binary))
+		(lcookie (make-temp-name
+			  (format "%s.%d." (user-login-name) (emacs-pid))))
 		file beg end)
 	    (w3m-process-with-environment
 		(list
+		 (cons "LOCAL_COOKIE" lcookie)
 		 (cons "QUERY_STRING"
-		       (encode-coding-string (w3m-url-to-file-name url)
-					     w3m-file-name-coding-system)))
+		       (format "dir=%s&cookie=%s"
+			       (encode-coding-string (w3m-url-to-file-name url)
+						     w3m-file-name-coding-system)
+			       lcookie)))
 	      (call-process w3m-dirlist-cgi-program nil t nil))
 	    (goto-char (point-min))
 	    (when (re-search-forward "^<html>" nil t)
