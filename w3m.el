4307c66e70854e78fcedd51953400535d51db05b
yamaoka <yamaoka>
2003-09-16 09:14:15 +0000

    * w3m-hist.el (w3m-history-backward): Return nil if there is no previous  element.
    * w3m.el (w3m-view-previous-page): Warn if there's no more history.

--- w3m.el
+++ w3m.el
@@ -138,7 +138,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.853 $"))
+    (let ((rev "$Revision: 1.854 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "1.3.%d"
 		   (- (string-to-number (match-string 1 rev)) 642)))))
@@ -4435,16 +4435,18 @@ forward is performed.  Otherwise, COUNT is treated as 1 by default."
 	;; Inhibit sprouting a new history.
 	(w3m-history-reuse-history-elements t)
 	(w3m-use-refresh nil))
-    (when hist
-      (w3m-goto-url (caar hist) nil nil
-		    (w3m-history-plist-get :post-data)
-		    (w3m-history-plist-get :referer)
-		    nil
-		    (w3m-history-element (caddr hist) t))
-      ;; Set the position pointers in the history.
-      (setcar w3m-history (cdr hist))
-      ;; Restore last position.
-      (w3m-history-restore-position))))
+    (if hist
+	(progn
+	  (w3m-goto-url (caar hist) nil nil
+			(w3m-history-plist-get :post-data)
+			(w3m-history-plist-get :referer)
+			nil
+			(w3m-history-element (caddr hist) t))
+	  ;; Set the position pointers in the history.
+	  (setcar w3m-history (cdr hist))
+	  ;; Restore last position.
+	  (w3m-history-restore-position))
+      (message "There's no more history"))))
 
 (defun w3m-view-next-page (&optional count)
   "View next page.  If COUNT is a positive integer, move forward COUNT
