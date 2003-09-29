40d45cdfaad2749b7fbafa502169f05cc10bbe97
ari <ari>
2003-09-29 14:53:01 +0000

    * w3m-symbol.el: Bind `w3m-output-coding-system' when compiling. (w3m-mule-unicode-symbol): New variable. (w3m-symbol): Add it for candidate. (w3m-symbol): Use it.
    * w3m.el (w3m-use-symbol): Check capability for
    `w3m-mule-unicode-symbol'.

--- w3m.el
+++ w3m.el
@@ -138,7 +138,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.868 $"))
+    (let ((rev "$Revision: 1.869 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "1.3.%d"
 		   (- (string-to-number (match-string 1 rev)) 642)))))
@@ -1015,7 +1015,14 @@ MIME CHARSET and CODING-SYSTEM must be symbol."
   :type 'boolean
   :require 'w3m-filter)
 
-(defcustom w3m-use-symbol (and (featurep 'mule) (eq w3m-type 'w3m-m17n))
+(defcustom w3m-use-symbol
+  (and (featurep 'mule)
+       (eq w3m-type 'w3m-m17n)
+       (or (null (eq w3m-output-coding-system 'utf-8))
+	   (and (charsetp 'mule-unicode-0100-24ff)
+		(charsetp 'mule-unicode-2500-33ff)
+		(or window-system (eq terminal-coding-system 'utf-8))))
+       t)
   "*Non-nil means replacing symbol."
   :group 'w3m
   :type 'boolean
