dbf4a888a40d1ac89a189c9cd42d1580bcd710b2
ari <ari>
2003-10-02 07:39:54 +0000

    (w3m-halfdump-command-arguments): Add "ucs_conv" option when using w3m-m17n and w3m-output-coding-system is utf-8; check w3m-output-coding-system at run time.

--- w3m.el
+++ w3m.el
@@ -138,7 +138,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.872 $"))
+    (let ((rev "$Revision: 1.873 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "1.3.%d"
 		   (- (string-to-number (match-string 1 rev)) 642)))))
@@ -1830,11 +1830,13 @@ for a refresh indication")
 	 (list "-halfdump"
 	       "-o" "ext_halfdump=1"
 	       "-o" "strict_iso2022=0"
+	       '(if (eq w3m-output-coding-system 'utf-8) "-o")
+	       '(if (eq w3m-output-coding-system 'utf-8) "ucs_conv=1")
 	       '(if charset "-I") 'charset
 	       "-O"
-	       (if (eq w3m-output-coding-system 'utf-8)
-		   "UTF-8"
-		 "ISO-2022-JP-2" )))
+	       '(if (eq w3m-output-coding-system 'utf-8)
+		    "UTF-8"
+		  "ISO-2022-JP-2")))
 	((eq w3m-input-coding-system 'w3m-euc-japan)
 	 (list "-halfdump" "-I" "e"))
 	(t (list "-halfdump")))
