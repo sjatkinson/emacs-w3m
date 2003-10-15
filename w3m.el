58770198e8e87e30989e8a18f6b9386a23f6cf43
yamaoka <yamaoka>
2003-10-15 00:40:11 +0000

    (w3m-decode-entities-string): Move `save-match-data' to the exterior of  `with-temp-buffer'.

--- w3m.el
+++ w3m.el
@@ -142,7 +142,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.884 $"))
+    (let ((rev "$Revision: 1.885 $"))
       (and (string-match "\\.\\([0-9]+\\) \$$" rev)
 	   (format "1.3.%d"
 		   (- (string-to-number (match-string 1 rev)) 642)))))
@@ -2974,10 +2974,11 @@ If optional RESERVE-PROP is non-nil, text property is reserved."
 
 (defun w3m-decode-entities-string (str)
   "Decode entities in the string STR."
-  (with-temp-buffer
-    (insert str)
-    (save-match-data (w3m-decode-entities))
-    (buffer-string)))
+  (save-match-data
+    (with-temp-buffer
+      (insert str)
+      (w3m-decode-entities)
+      (buffer-string))))
 
 (defun w3m-encode-specials-string (str)
   "Encode special characters in the string STR."
