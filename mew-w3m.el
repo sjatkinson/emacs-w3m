02f30bc0cc32b0c8434b1fbbdfc4a4ee5807513f
tsuchiya <tsuchiya>
2003-11-24 08:46:52 +0000

    Merge changes on experimental-async branch to the main trunk.

--- mew-w3m.el
+++ mew-w3m.el
@@ -5,7 +5,7 @@
 ;; Author: Shun-ichi GOTO  <gotoh@taiyo.co.jp>,
 ;;         Hideyuki SHIRAI <shirai@meadowy.org>
 ;; Created: Wed Feb 28 03:31:00 2001
-;; Version: $Revision: 1.48 $
+;; Version: $Revision: 1.49 $
 ;; Keywords: Mew, mail, w3m, WWW, hypermedia
 
 ;; This file is a part of emacs-w3m.
