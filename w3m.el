69c85de07423e51fe01f9ca4afb4123b9000f943
tsuchiya <tsuchiya>
2003-11-27 14:48:03 +0000

    * w3m-antenna.el: Define a new minor mode `w3m-antenna-mode'. (w3m-antenna-mode-map, w3m-antenna-mode): New variables. (w3m-antenna-mode, w3m-antenna-mode-setter, w3m-antenna-edit): New functions.
    * w3m-bookmark.el (w3m-bookmark-mode): Change its interactive
    spec, and improve its document.
    
    * w3m.el (w3m-minor-mode): Change its interactive spec.

--- w3m.el
+++ w3m.el
@@ -146,7 +146,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.945 $"))
+    (let ((rev "$Revision: 1.946 $"))
       (and (string-match "\\.\\([0-9]+\\) \\$\\'" rev)
 	   (format "1.3.%d"
 		   (- (string-to-number (match-string 1 rev)) 642)))))
@@ -7952,7 +7952,7 @@ FROM-COMMAND is defined to `w3m-minor-mode-map' for same keys in
 
 (defun w3m-minor-mode (&optional arg)
   "Minor mode to view text/html part in articles."
-  (interactive)
+  (interactive "P")
   (prog1 (setq w3m-minor-mode
 	       (if arg
 		   (> (prefix-numeric-value arg) 0)
