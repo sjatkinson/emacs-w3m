48b1d9ce93f5d59534c8c9fe0436b2a62d9e687c
yamaoka <yamaoka>
2003-12-19 08:17:36 +0000

    (w3m-toggle-inline-images): Ignore `w3m-safe-url-regexp' when the prefix  argument is given.

--- w3m.el
+++ w3m.el
@@ -148,7 +148,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.964 $"))
+    (let ((rev "$Revision: 1.965 $"))
       (and (string-match "\\.\\([0-9]+\\) \\$\\'" rev)
 	   (format "1.3.%d"
 		   (- (string-to-number (match-string 1 rev)) 642)))))
@@ -3072,8 +3072,13 @@ appearance of images in the article buffer."
    (let ((status w3m-display-inline-images))
      (unless (w3m-display-graphic-p)
        (error "Can't display images in this environment"))
-     (if force (setq w3m-display-inline-images nil
-		     status nil))
+     (if (and force
+	      (yes-or-no-p "\
+Are you sure you really want to show all images (maybe unsecure)? "))
+	 ;; `w3m-safe-url-regexp' is already bound by `w3m-with-w3m-buffer'.
+	 (setq w3m-safe-url-regexp nil
+	       w3m-display-inline-images nil
+	       status nil))
      (unwind-protect
 	 (w3m-toggle-inline-images-internal (if w3m-display-inline-images
 						'on 'off)
