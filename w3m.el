4fc6d34550193b96a22ff152a1b6d67fe8869610
yamaoka <yamaoka>
2004-04-27 12:29:05 +0000

    (w3m-pixels-per-line, w3m-pixels-per-character): Doc fix.

--- w3m.el
+++ w3m.el
@@ -150,7 +150,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.1027 $"))
+    (let ((rev "$Revision: 1.1028 $"))
       (and (string-match "\\.\\([0-9]+\\) \\$\\'" rev)
 	   (setq rev (- (string-to-number (match-string 1 rev)) 1006))
 	   (concat "1.3.85" (if (> rev 0) (format ".%d" rev) "")))))
@@ -389,7 +389,10 @@ and lines which images occupy."
 (defcustom w3m-pixels-per-line 64
   "*Integer used for the `-ppl' argument of the w3m command.
 If nil, the height of the default face is used.  It is valid only when
-`w3m-treat-image-size' is non-nil."
+`w3m-treat-image-size' is non-nil.  Note that a small value may not
+induce a good result.  If you want to use emacs-w3m in a character
+terminal and make `w3m-treat-image-size' effective, you need to set
+this variable properly."
   :group 'w3m
   :type '(choice (const :tag "Auto Detect" nil)
 		 (integer :tag "Specify Pixels")))
@@ -397,7 +400,9 @@ If nil, the height of the default face is used.  It is valid only when
 (defcustom w3m-pixels-per-character nil
   "*Integer used for the `-ppc' argument of the w3m command.
 If nil, the width of the default face is used.  It is valid only when
-`w3m-treat-image-size' is non-nil."
+`w3m-treat-image-size' is non-nil.  If you want to use emacs-w3m in a
+character terminal and make `w3m-treat-image-size' effective, you need
+to set this variable properly."
   :group 'w3m
   :type '(radio (const :tag "Auto Detect" nil)
 		(integer :format "Specify Pixels: %v\n" :size 0)))
