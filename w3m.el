ae952d5a69e1e22165fdc01647ed5d7d036eff72
yamaoka <yamaoka>
2004-05-11 10:05:14 +0000

    Fix again and again. ^^;;

--- w3m.el
+++ w3m.el
@@ -150,7 +150,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.1036 $"))
+    (let ((rev "$Revision: 1.1037 $"))
       (and (string-match "\\.\\([0-9]+\\) \\$\\'" rev)
 	   (setq rev (- (string-to-number (match-string 1 rev)) 1030))
 	   (concat "1.4.0" (if (>= rev 0) (format ".%d" (+ rev 50)) "")))))
@@ -4125,8 +4125,7 @@ If the optional argument NO-CACHE is non-nil, cache is not used."
 	    (when (search-forward "\n\n" nil t)
 	      ;; Asahi-shimbun sometimes says gif as jpeg mistakenly.  So,
 	      ;; we cannot help trusting the magic at the beginning of data.
-	      (when (looking-at "\
-\\(GIF8\\)\\|\\(\377\330\377[\340\356]\\)\\|\\(\211PNG\\)")
+	      (when (looking-at "\\(GIF8\\)\\|\\(\377\330\\)\\|\\(\211PNG\\)")
 		(setq type (cond ((match-beginning 1) "gif")
 				 ((match-beginning 2) "jpeg")
 				 ((match-beginning 3) "png")))
