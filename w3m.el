7cead915df0e0cbb12a52338b0c99119dcbe4585
yamaoka <yamaoka>
2004-05-11 07:58:50 +0000

    (w3m-w3m-dump-extra): Trust the magic at the beginning of image data rather  than the header.

--- w3m.el
+++ w3m.el
@@ -150,7 +150,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.1033 $"))
+    (let ((rev "$Revision: 1.1034 $"))
       (and (string-match "\\.\\([0-9]+\\) \\$\\'" rev)
 	   (setq rev (- (string-to-number (match-string 1 rev)) 1030))
 	   (concat "1.4.0" (if (>= rev 0) (format ".%d" (+ rev 50)) "")))))
@@ -4118,10 +4118,24 @@ If the optional argument NO-CACHE is non-nil, cache is not used."
       (w3m-message "Reading %s...done" url)
       (when success
 	(goto-char (point-min))
-	(let ((case-fold-search t))
+	(let ((case-fold-search t)
+	      type)
 	  (when (re-search-forward "^w3m-current-url:" nil t)
 	    (delete-region (point-min) (match-beginning 0))
 	    (when (search-forward "\n\n" nil t)
+	      ;; Asahi-shimbun sometimes says gif as jpeg mistakenly.  So,
+	      ;; we cannot help trusting the magic at the beginning of data.
+	      (when (looking-at "\
+\\(GIF8\\)\\|\\(\211PNG\\)\\|\\(\377\330\377\\(\340\356\\)\\)")
+		(setq type (cond ((match-beginning 1) "gif")
+				 ((match-beginning 2) "png")
+				 ((match-beginning 1) "jpeg")))
+		(when (re-search-backward "^content-type: image/\\(.+\\)$"
+					  nil t)
+		  (delete-region (goto-char (match-beginning 1))
+				 (match-end 0))
+		  (insert type)
+		  (search-forward "\n\n")))
 	      (let ((header (buffer-substring (point-min) (point))))
 		(when w3m-use-cookies
 		  (w3m-cookie-set url (point-min) (point)))
