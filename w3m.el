5db73fa3fb4f6036586404c9270d4b0019e22c62
yamaoka <yamaoka>
2008-05-28 23:36:15 +0000

    (w3m-examine-command-line-args): Treat the null string given as a url in the  command line args as `w3m-home-page' or "about:".

--- w3m.el
+++ w3m.el
@@ -176,7 +176,7 @@
 
 (defconst emacs-w3m-version
   (eval-when-compile
-    (let ((rev "$Revision: 1.1363 $"))
+    (let ((rev "$Revision: 1.1364 $"))
       (and (string-match "\\.\\([0-9]+\\) \\$\\'" rev)
 	   (setq rev (- (string-to-number (match-string 1 rev)) 1136))
 	   (format "1.4.%d" (+ rev 50)))))
@@ -8953,7 +8953,11 @@ defaults to the value of `w3m-home-page' or \"about:\"."
 	(directives '("-f" "-funcall" "--funcall" "-e"))
 	args)
     (if (and url (not (string-match "\\`-" url)))
-	(setq command-line-args-left (cdr command-line-args-left))
+	(progn
+	  (setq command-line-args-left (cdr command-line-args-left))
+	  (when (string-match "\\`[\t ]*\\'" url)
+	    ;; emacs -f w3m '' ...
+	    (setq url (or w3m-home-page "about:"))))
       (setq args (nthcdr (max (- (length command-line-args)
 				 (length command-line-args-left)
 				 2)
